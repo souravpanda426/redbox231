7309b0afbb5eeba59f283f039ca6c7d285e68362
souravpanda426 <panda.sourav426@gmail.com>
2023-01-25 11:57:21 +0530

    java program from string to integer

---
+++ PRACTICEPROJECT/src/mypackage/StringToInteger.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class StringToInteger {
+
+	public static void main(String[] args) {
+		
+		
+		String x = "45";
+		int y = Integer.parseInt(x);
+		System.out.println(y);
+	}
+
+}
