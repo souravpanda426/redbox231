6e73bd9d94157593802dc6effc9566a286be791b
souravpanda426 <panda.sourav426@gmail.com>
2023-01-25 12:10:11 +0530

    Integer2String

---
+++ PRACTICEPROJECT/src/mypackage/Integer2String.java
@@ -0,0 +1,18 @@
+package mypackage;
+
+public class Integer2String {
+
+	public static void main(String[] args) {
+	
+		
+		int x = 78;
+		
+		String y = String.valueOf(x);
+		System.out.println(y);
+
+		
+		
+
+	}
+
+}
