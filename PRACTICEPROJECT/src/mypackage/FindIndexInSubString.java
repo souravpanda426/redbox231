a5dc27d027719ef9329b8e0eaa4e91f7d847d2b0
souravpanda426 <panda.sourav426@gmail.com>
2023-02-04 00:22:43 +0530

    committing addTwoMatrix

---
+++ PRACTICEPROJECT/src/mypackage/FindIndexInSubString.java
@@ -0,0 +1,17 @@
+package mypackage;
+
+public class FindIndexInSubString {
+
+	public static void main(String[] args) {
+	
+
+		
+		
+		String x = "this is index of example";
+		String y = "index";
+		System.out.println(x.indexOf(y));
+		
+
+	}
+
+}
