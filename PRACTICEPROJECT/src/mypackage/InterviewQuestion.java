ac06e2bbf84e96e4133c96c4e64ff205b3fe6abf
souravpanda426 <panda.sourav426@gmail.com>
2023-02-05 11:31:16 +0530

    java program

---
+++ PRACTICEPROJECT/src/mypackage/InterviewQuestion.java
@@ -0,0 +1,43 @@
+package mypackage;
+
+public class InterviewQuestion {
+
+	public static void main(String[] args) {
+	
+		
+		int finalvalue=0;
+
+		
+		String[] st =  {"ABC12","WAG983","89UIO"};
+		
+		for (String string : st) {
+			int sum = Sum(string);
+			
+			finalvalue = finalvalue + sum;
+		}
+		
+		System.out.println(finalvalue);
+		
+	}
+	
+	
+	
+	private static int Sum(String x) {
+		
+		int sum=0;
+		
+		for(int i =0;i<x.length();i++) {
+			char ch = x.charAt(i);
+			
+			if(ch>='1' & ch<='9') {
+				sum = sum + (ch-48);
+				
+				
+			}
+		}
+		return sum;
+		
+		
+	}
+
+}
