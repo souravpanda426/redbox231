463399e9ae562e6f8aedd4df0cad85252aec629e
souravpanda426 <panda.sourav426@gmail.com>
2023-02-06 17:54:30 +0530

    java program for implicit type casting

---
+++ PRACTICEPROJECT/src/mypackage/ImplicityTypeCasting.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class ImplicityTypeCasting {
+
+	public static void main(String[] args) {
+		
+
+		byte b = 'd';
+		char f = (char) b;
+		System.out.println(f);
+
+	}
+
+}
