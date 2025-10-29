03537c35781c8156eaab3d91d1665e6ec3251177
souravpanda426 <Panda.sourav426@gmail.com>
2025-10-29 13:35:37 +0530

    excpetion

---
+++ PRACTICEPROJECT/src/NotEligibleToVote.java
@@ -0,0 +1,20 @@
+
+public class NotEligibleToVote{
+
+	
+	public static void main(String[] args) throws NotabletoVote {
+		
+		int age = 17;
+		
+		if(age<18) {
+			
+			throw new NotabletoVote();
+		}
+	
+	
+	
+	
+
+}
+	
+}
